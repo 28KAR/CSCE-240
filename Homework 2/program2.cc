7247f4a9f4b281aa66b5340e1b1d7797c2594784
28KAR <car28@email.sc.edu>
2025-09-28 02:26:45 -0400

    Finish Homework 2

--- Homework 2/program2.cc
+++ Homework 2/program2.cc
@@ -11,4 +11,49 @@ using std::endl;
 
 int main() {
 
+    int month1, day1, year1, month2, day2, year2;
+
+    cin >> month1 >> day1 >> year1 >> month2 >> day2 >> year2;
+
+    if (!ValidDate(month1, day1, year1)) {
+
+        cout << month1 << "/" << day1 << "/" << year1 << " is not a valid date" << endl;
+        return 0;
+        
+    }
+    if (!ValidDate(month2, day2, year2)) {
+
+        cout << month2 << "/" << day2 << "/" << year2 << " is not a valid date" << endl;
+        return 0;
+
+    }
+    
+    int monthTrack = month1;
+    int dayTrack = day1;
+    int yearTrack = year1;
+
+    int days = 0;
+
+    while (!((monthTrack == month2)&&(dayTrack == day2)&&(yearTrack == year2))) {
+        if ((yearTrack < year2) || ((yearTrack == year2) && (monthTrack < month2)) || ((yearTrack == year2) && (monthTrack < month2) && dayTrack < day2)) {
+
+            days++;
+            NextDate(monthTrack, dayTrack, yearTrack);
+
+        }
+        if ((yearTrack > year2) || ((yearTrack == year2) && (monthTrack > month2)) || ((yearTrack == year2) && (monthTrack > month2) && dayTrack > day2)) {
+
+            days--;
+            PreviousDate(monthTrack, dayTrack, yearTrack);
+            
+        }
+    }
+
+    if (days < 0)
+        cout << month1 << "/" << day1 << "/" << year1 << " is " << (days*(-1)) << " days after " << month2 << "/" << day2 << "/" << year2 << endl;
+    else
+        cout << month1 << "/" << day1 << "/" << year1 << " is " << (days) << " days before " << month2 << "/" << day2 << "/" << year2 << endl;
+
+    return 0;
+
 }
