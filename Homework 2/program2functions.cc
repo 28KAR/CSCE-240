33efcb9d22e8e5587e0b2a276cc10539ef7d9da5
28KAR <car28@email.sc.edu>
2025-09-27 22:54:10 -0400

    Homework 2 Progress

--- Homework 2/program2functions.cc
+++ Homework 2/program2functions.cc
@@ -4,3 +4,32 @@
                          // style - use "" for user defined headers
                          //         and <> for standard library headers
 
+bool LeapYear(int year) {
+
+return ((year % 4) == 0);
+
+}
+
+int LastDayOfMonth(int year, int month) {
+
+
+
+}
+
+bool ValidDate(int year, int month, int day) {
+
+
+
+}
+
+void NextDate(int& year, int& month, int& day) {
+
+
+
+}
+
+void PreviousDate(int& year, int& month, int& day) {
+
+
+
+}
