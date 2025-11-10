b09d3d3e8aa287649b364499e031e498d597327b
28KAR <car28@email.sc.edu>
2025-11-10 17:17:03 -0500

    Homework5

---
+++ Homework5/songRecording.cc
@@ -0,0 +1,11 @@
+//  -------------------------------------  //
+//  Copyright Katherine Asher Rogers 2025  //
+//  -------------------------------------  //
+#include"songRecording.h"
+#include<iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+using std::ostream;
+
---
+++ Homework5/songRecording.h
@@ -0,0 +1,9 @@
+//  -------------------------------------  //
+//  Copyright Katherine Asher Rogers 2025  //
+//  -------------------------------------  //
+#ifndef SONGRECORDING_H_
+#define SONGRECORDING_H_
+
+
+
+#endif
